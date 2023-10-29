e32dda70a53c30aa24cfb3cfeb79ea1b239099e5
Daniel Washington Ignacio <danielvertigo@hotmail.com>
2023-10-29 10:50:34 -0300

    Initial Commit

---
+++ RestartUI/RestartUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartUIApp.swift
+//  RestartUI
+//
+//  Created by Daniel Washington Ignacio on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
